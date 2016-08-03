92562d11e78cc2e3cdd21bd7bc33e40e32737a81
yaoqiang <qiang.yao@mtime.com>
2016-08-03 13:24:06 +0800

    about cron

--- app/server/server.js
+++ app/server/server.js
@@ -154,14 +154,14 @@ server.listen(port);
 //start crontab. 以后有空迁移改造..
 var noticeList = ['亲爱的各位玩家，绿色游戏，禁止作弊，一旦发现立即惩罚。净化游戏，提升游戏乐趣，从我做起！', '本月股神榜重磅开启，下月1号上榜就赢奖！约起来冲起来！', '亲爱的各位玩家，关注微信公众号:大同扎股子，参与苹果商店五星好评活动，立即获得10个大喇叭！'];
 var cron = require('node-cron');
-cron.schedule('*/5 * * * * *', function() {
+cron.schedule('* */5 * * * *', function() {
   // console.log('You will see this message every second');
 
-  var notice = Math.floor(Math.random() * noticeList.length);
+  var noticeIndex = Math.floor(Math.random() * noticeList.length);
 
   superAgent.post('http://127.0.0.1:4000/api/game/sendBBS')
       .set('Content-Type', 'application/json')
-      .send({content: notice})
+      .send({content: noticeList[noticeIndex]})
       .end((err, res)=> {
       });
 });
